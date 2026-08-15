b7a71f26dd838e9da65dc9f0e37ddd947761f47d
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-08-14 19:24:24 -0500

    Ejercio sobre if anidado e if compuesto

---
+++ Clase 14 de agosto 2026/js.js
@@ -0,0 +1,31 @@
+let numero = Number( prompt("Ingrese un numero") );
+
+//if anidado
+if( numero > 0){
+    if((numero % 2) == 0){
+        alert("El numero "+numero+" es positivo par");
+    } else {
+        alert("El numero "+numero+" es positivo impar");
+    }
+} else if(numero < 0) {
+    if((numero % 2) == 0){
+        alert("El numero "+numero+" es negativo par");
+    } else {
+        alert("El numero "+numero+" es negativo impar");
+    }
+} else {
+     alert("El numero "+numero+" es neutro");
+}
+
+//if compuesto
+if( numero > 0 && (numero % 2) == 0){
+    alert("El numero "+numero+" es positivo par");
+} else if(numero > 0 && (numero % 2) != 0){
+    alert("El numero "+numero+" es positivo impar");
+} else if(numero < 0 && (numero % 2) == 0) {
+    alert("El numero "+numero+" es negativo par");
+} else if(numero < 0 && (numero % 2) != 0){
+    alert("El numero "+numero+" es negativo impar");
+} else {
+     alert("El numero "+numero+" es neutro");
+}
