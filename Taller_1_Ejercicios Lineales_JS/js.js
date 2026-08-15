cfbd3b70a3ed722c5529248a0e3aaeb10f4759bd
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-08-14 22:41:36 -0500

    Taller ejercicios lineales numeral 1

--- Taller_1_Ejercicios Lineales_JS/js.js
+++ Taller_1_Ejercicios Lineales_JS/js.js
@@ -1,1 +1,11 @@
+//Ejercicios Lineales.
+
+//#1
 let salario = Number( prompt("Salario base") );
+let salud = Math.round( salario * 0.04 );
+let pension = Math.round( salario * 0.07 );
+let neto = salario - (salud + pension);
+alert("Su salario base es de "+salario+" pero tiene descuentos por salud del 4% equvalente a "+salud+" y pension del 7% equivalente a "+pension+" para un total neto de "+neto);
+
+
+//#2
