538f6a6354ce02b8eda4b2529f5c65fc6bcb0432
Luis Felipe Ochoa Cardona <luisfelipeochoac@gmail.com>
2026-08-21 19:28:39 -0500

    Ejercicio de ciclo while y for pero con prompt y tipos de variables contadora y acumuladoras

--- Clase 21 de agosto 2026/ciclos.js
+++ Clase 21 de agosto 2026/ciclos.js
@@ -16,11 +16,36 @@ for (let i = 0; i < 5; i++) {
 //el atajo del teclado para comentar varias lineas de codigo es: Ctrl + K + C
 //el atajo del teclado para descomentar varias lineas de codigo es: Ctrl + K + U
 
-let inicio = Number(prompt("Ingrese valor de inicio del ciclo:"));
-let fin = Number(prompt("Ingrese valor de fin del ciclo:"));
-let intervalo = Number(prompt("Ingrese valor de incremento del ciclo:"));
+document.write("<hr>");
 
-while (inicio <= fin) {
-    document.write("El valor de inicio es: " + inicio + "<br>");
-    inicio += intervalo;
+// let inicio = Number(prompt("Ingrese valor de inicio del ciclo:"));
+// let fin = Number(prompt("Ingrese valor de fin del ciclo:"));
+// let intervalo = Number(prompt("Ingrese valor de incremento del ciclo:"));
+
+// if (inicio < fin) {
+//     while (inicio <= fin) {
+//         document.write("El valor de inicio es: " + inicio + "<br>");
+//         inicio += intervalo;
+//     }
+// } else if (inicio > fin) {
+//     while (inicio >= fin) {
+//         document.write("El valor de inicio es: " + inicio + "<br>");
+//         inicio -= intervalo;
+//     }
+// }
+
+// document.write("<hr>");
+
+//variables contadoras y acumuladoras
+let numero = Number(prompt("Ingrese total de numeros:"));
+let num;
+let suma = 0;
+let totalNum = 0;
+for (let i = 1; i <= numero; i++) {
+    num = Number(prompt("Ingrese el numero para este ciclo (" + i + "):"));
+    document.write(num+ " ");
+    suma += num;
+    totalNum++;
 }
+document.write("<br>La suma de los numeros es: " + suma);
+document.write("<br>El total de numeros ingresados es: " + totalNum);
