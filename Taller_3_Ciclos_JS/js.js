b30518483cd2363431c6970214004c972116281d
Luis Felipe Ochoa Cardona <luisfelipeochoac@gmail.com>
2026-08-28 15:13:49 -0500

    Taller 3 ejercicio 4 y 5

--- Taller_3_Ciclos_JS/js.js
+++ Taller_3_Ciclos_JS/js.js
@@ -52,3 +52,25 @@ while (true) {
 }
 
 alert("Resultado Ejercicio 3:\nCantidad de números ingresados: " + cantidadNumeros + "\nSuma total: " + sumaTotal);
+
+
+// #4
+let numeroTabla = parseInt(prompt("Ingrese el número para generar la tabla de multiplicar hasta el 30:"));
+let tablaMultiplicar = "Tabla de multiplicar del " + numeroTabla + " (hasta el 30):\n";
+
+for (let i = 1; i <= 30; i++) {
+    tablaMultiplicar += numeroTabla + " x " + i + " = " + (numeroTabla * i) + "\n";
+}
+
+alert(tablaMultiplicar);
+
+
+// #5
+let numeroDesc = parseInt(prompt("Ingrese el número para generar la tabla de multiplicar descendente:"));
+let tablaDescendente = "Tabla de multiplicar descendente del " + numeroDesc + " (del 30 al 1):\n";
+
+for (let i = 30; i >= 1; i--) {
+    tablaDescendente += numeroDesc + " x " + i + " = " + (numeroDesc * i) + "\n";
+}
+
+alert(tablaDescendente);
